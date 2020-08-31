8a03954d74a54dd6415e328f5659e83ffe20e784
Dmitry Post <dmitrypost@live.com>
2020-08-31 17:54:25 -0400

    add html formatter tests

---
+++ TestNinjaTests/HtmlFormatterTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaTests
+{
+    public class HtmlFormatterTests
+    {
+        [TestCase("Some content")]
+        public void FormatAsBold_GivenString_ReturnsGivenStringWrappedAroundStrong(string content)
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold(content);
+
+            Assert.IsTrue(result.StartsWith("<strong>"));
+            Assert.IsTrue(result.EndsWith("</strong>"));
+            Assert.IsTrue(result.Contains(content));
+        }
+    }
+}

fa1f2f705832a4afbf680c6092ef41470528af13
Dmitry Post <dmitrypost@live.com>
2020-08-30 22:34:24 -0400

    ErrorLogger tests added

---
+++ TestNinjaTests/ErrorLoggerTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaTests
+{
+    public class ErrorLoggerTests
+    {
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase(" ")]
+        public void Log_GivenNullOrWhiteSpace_ExpectsArgumentNullExceptionThrown(string error)
+        {
+            var logger = new ErrorLogger();
+
+            
+            Assert.Throws<ArgumentNullException>(() => logger.Log(error));
+        }
+
+        [TestCase("fdsafdsaf")]
+        public void Log_GivenErrorMessage_ExpectsErrorMessage(string errorMessage)
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log(errorMessage);
+
+            Assert.IsTrue(logger.LastError.Equals(errorMessage));
+        }
+
+        [TestCase("error occurred")]
+        public void Log_GivenErrorMessage_RaisesEvent(string errorMessage)
+        {
+            var logger = new ErrorLogger();
+
+            var guid = Guid.Empty;
+            logger.ErrorLogged += delegate(object? sender, Guid e) { guid = e; };
+            logger.Log(errorMessage);
+
+            Assert.That(guid != Guid.Empty);
+        }
+    }
+}
